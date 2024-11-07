9a99e458741b3cec173db2743151c1890070395a
lnidogon <lovro.nidogon@gmail.com>
2024-11-07 01:46:46 +0100

    Dodan chakra provider

---
+++ src/frontend/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Card, Flex } from "@chakra-ui/react";
+import { Text, Button, Input } from "@chakra-ui/react";
+export default function LoginPage() {
+  return (
+    <>
+      <Text> Bok </Text>
+      <Flex as="form">
+        <Input type="password"></Input>
+      </Flex>
+    </>
+  );
+}
