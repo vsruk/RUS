471d56593032afe471fb0090f2f85719e59505d6
lnidogonfer <lovro.nidogon@fer.unizg.hr>
2024-11-07 13:36:10 +0100

    Dodan password input

--- src/frontend/src/components/features/LoginForm/LoginForm.tsx
+++ src/frontend/src/components/features/LoginForm/LoginForm.tsx
@@ -1,19 +1,23 @@
 import { InputGroup } from "@/components/ui/input-group";
-import { Flex, Text, Input, Heading } from "@chakra-ui/react";
+import { Flex, Text, Input, Heading, Button } from "@chakra-ui/react";
 
 export const LoginForm = () => {
   return (
     <>
       <Flex
+        padding="20px"
         borderRadius="10px"
-        bg="gray"
+        bg="gray.300"
         direction="column"
         alignItems="center"
         as="form"
         height="100%"
         width="60%"
+        gapY="10px"
       >
-        <Heading>Login</Heading>
+        <Heading marginBottom="20px" fontSize="2xl">
+          Login
+        </Heading>
         <Flex
           direction="row"
           width="80%"
@@ -21,8 +25,20 @@ export const LoginForm = () => {
           alignItems="center"
         >
           <Text>e-mail:</Text>
-          <Input type="email" width="80%" />
+          <Input type="email" width="80%" borderColor="black" />
         </Flex>
+        <Flex
+          direction="row"
+          width="80%"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text>Lozinka:</Text>
+          <Input type="password" width="80%" borderColor="black" />
+        </Flex>
+        <Button color="white" bg="gray" marginTop="20px" width="200px">
+          Log In
+        </Button>
       </Flex>
     </>
   );
