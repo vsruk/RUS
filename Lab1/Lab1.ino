d69c68119bcd738b91bc4908bb9a8106f24dd22d
vsruk <vlado.sruk@tvz.hr>
2025-04-18 11:00:11 +0200

    Update Lab1.ino

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,173 +1,273 @@
-#include <Arduino.h>
+/**
+ * @file
+ * @brief Simulates interrupts on an Arduino Mega (Wokwi project).
+ *
+ * This project demonstrates how to simulate external and timer interrupts using an Arduino Mega.
+ * It uses three buttons (BUTTON0, BUTTON1, BUTTON2) to trigger external interrupts and a timer
+ * (via Timer1) for periodic events. The program also measures distance using an ultrasonic sensor
+ * and triggers a distance alert if an object is too close.
+ *
+ * Various LEDs indicate the interrupt events and sensor/timer actions. Additionally, several
+ * digital pins are toggled to simulate signals for a logic analyzer.
+ *
+ * @note This code is designed for use with the Wokwi simulation environment.
+ */
 
-#define BUTTON1 6   // Tipkalo 1 - koristi digitalRead()
-#define BUTTON2 3   // Tipkalo 2 - vanjski prekid 1 (INT1)
-#define TRIG 4      // HC-SR04 Trig pin
-#define ECHO 2      // HC-SR04 Echo pin (prekid 0) (INT0 - Viši prioritet od INT1)
-#define LED 13      // LED za tajmerski prekid
+#include <avr/interrupt.h>
 
-volatile bool button2_pressed = false;
-volatile bool sensor_triggered = false;
-volatile bool timer_flag = false;
-volatile bool nested_occurred = false; // Novi flag za detekciju preklapanja
-volatile unsigned long echo_start_time = 0; // Vrijeme početka ECHO signala
-volatile unsigned long echo_duration = 0;   // Trajanje ECHO signala
+// Pin definitions
+#define BUTTON0      2
+#define BUTTON1      3
+#define BUTTON2      21
+#define LED_INT0     8
+#define LED_INT1     9
+#define LED_INT2     10
+#define LED_Sensor   11
+#define LED_Timer    12
+#define TRIG_PIN     6
+#define ECHO_PIN     5
 
-bool lastButton1State = HIGH;  // Za praćenje stanja tipkala 1
+#define TIMER1_PRESCALER   1024
+#define TIMER1_COMPARE     15624
+#define BLINK_INTERVAL     200
+#define DEBOUNCE_DELAY     50
+#define TIMER_DELAY        1000
+#define ALARM_DISTANCE     100
 
-// ISR za tipkalo 2 (pokreće slanje ultrazvučnog impulsa) - Srednji prioritet
-void ISR_button2() {
-  sei(); // Omogući nested interrupts
+// Logic analyzer pin definitions
+#define LOGIC_ANALYZER_PIN0 14 
+#define LOGIC_ANALYZER_PIN1 15 
+#define LOGIC_ANALYZER_PIN2 16 
+#define LOGIC_ANALYZER_TIMER 17
+#define LOGIC_ANALYZER_SENSOR 18 
 
-  // Pošalji TRIG impuls samo kada je gumb pritisnut
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2); // Kratka pauza, u redu unutar ISR
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);// Kratka pauza, u redu unutar ISR
-  digitalWrite(TRIG, LOW);
+// Global volatile variables used in ISRs and main loop
+volatile bool     intFlag[3] = {0};
+volatile unsigned long lastInterruptTime[3] = {0}, lastTimerTime = 0;
+volatile bool     distanceAlert = 0, timerFlag = 0;
+volatile bool     pastDistanceAlert;
 
-  button2_pressed = true;
-  // Serial.println("DEBUG: ISR_button2 finished"); // Može se dodati za debug
-}
+/**
+ * @brief Arduino setup function.
+ *
+ * Configures the pin modes for LEDs, buttons, ultrasonic sensor, and logic analyzer pins.
+ * Attaches external interrupts for BUTTON0, BUTTON1, and BUTTON2.
+ * Sets up Timer1 to generate an interrupt when it reaches the compare value.
+ * Initializes serial communication and enables interrupts.
+ */
+void setup(){
+    pinMode(LED_INT0,    OUTPUT);
+    pinMode(LED_INT1,    OUTPUT);
+    pinMode(LED_INT2,    OUTPUT);
+    pinMode(LED_Sensor,  OUTPUT);
+    pinMode(LED_Timer,   OUTPUT);
 
-// ISR za Echo signal s HC-SR04 (detektira reflektirani val) - Najviši prioritet
-void ISR_sensor() {
-  sei(); // Omogući nested interrupts
-  
-  // Ako je ovo RISING edge, počinjemo mjeriti vrijeme
-  if (digitalRead(ECHO) == HIGH) {
-    echo_start_time = micros();
-  } else {
-    // Ako je FALLING edge, računamo trajanje
-    echo_duration = micros() - echo_start_time;
-    sensor_triggered = true;
-  }
+    pinMode(BUTTON0, INPUT_PULLUP);
+    pinMode(BUTTON1, INPUT_PULLUP);
+    pinMode(BUTTON2, INPUT_PULLUP);
 
-  // Ako je ovaj ISR pozvan dok je drugi (npr. Timer) bio aktivan,
-  // ovo će se postaviti. Provjeravamo u loop().
-  nested_occurred = true;
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
 
-  // Serial.println("DEBUG: ISR_sensor finished"); // Može se dodati za debug
-}
+    pinMode(LOGIC_ANALYZER_PIN0, OUTPUT);
+    pinMode(LOGIC_ANALYZER_PIN1, OUTPUT);
+    pinMode(LOGIC_ANALYZER_PIN2, OUTPUT);
+    pinMode(LOGIC_ANALYZER_TIMER, OUTPUT);
+    pinMode(LOGIC_ANALYZER_SENSOR, OUTPUT);
 
-// ISR za Timer1 (LED treptanje) - Najniži prioritet od ova tri
-ISR(TIMER1_COMPA_vect) {
-  sei(); // Omogući nested interrupts (ključno za test)
+    attachInterrupt(digitalPinToInterrupt(BUTTON0), ISR_INT0, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON1), ISR_INT1, FALLING);
+    attachInterrupt(digitalPinToInterrupt(BUTTON2), ISR_INT2, FALLING);
 
-  timer_flag = true; // Signaliziraj loop-u da je vrijeme za toggle LED
-}
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("\n--- Arduino Prekidi Test ---");
-  Serial.println("Prioriteti (HW): ECHO(INT0) > BUTTON2(INT1) > Timer1");
-  Serial.println("Nested interrupts su OMOGUĆENI u ISR-ovima.");
-  Serial.println("Ultrazvučni senzor će ispisivati udaljenost samo kada je < 100cm");
-
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT_PULLUP);
-  pinMode(TRIG, OUTPUT);
-  digitalWrite(TRIG, LOW); // Osiguraj da je TRIG isključen na početku
-  pinMode(ECHO, INPUT);
-  pinMode(LED, OUTPUT);
-
-  // Vanjski prekidi
-  attachInterrupt(digitalPinToInterrupt(BUTTON2), ISR_button2, FALLING); // INT1
-  attachInterrupt(digitalPinToInterrupt(ECHO), ISR_sensor, CHANGE);      // INT0, mijenjamo na CHANGE da uhvatimo i rising i falling edge
-
-  // Timer1 konfiguracija za LED treptanje (1 Hz)
-  cli(); // Isključi prekide tijekom podešavanja
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
-  OCR1A = 15624; // 1 sekunda (16 MHz / 1024 / 1Hz - 1)
-  TCCR1B |= (1 << WGM12); // CTC mod
-  TCCR1B |= (1 << CS12) | (1 << CS10); // prescaler 1024
-  TIMSK1 |= (1 << OCIE1A); // enable compare match interrupt
-  sei(); // Omogući prekide globalno
-}
-
-void loop() {
-  bool processed_nested = false;
-
-  // Obrada flag-a postavljenog u ISR_sensor
-  // Koristimo noInterrupts/interrupts da osiguramo atomičnost čitanja i resetiranja flag-a
-  noInterrupts();
-  if (nested_occurred) {
-    processed_nested = true; // Zapamti da smo ovo obradili
-    nested_occurred = false; // Resetiraj flag
-    interrupts(); // OMOGUĆI PREKIDE što prije
-
-    Serial.println("### LOOP: DETEKTIRANO PREKLAPANJE (nested_occurred flag) ###");
-    // Ovdje možemo dodati dodatnu logiku ako je potrebno znati da se preklapanje dogodilo
-  } else {
-    interrupts(); // Obavezno omogući prekide i ako flag nije bio postavljen
-  }
-
-  // Tipkalo 1 se čita ručno, za dodatno testiranje
-  bool button1State = digitalRead(BUTTON1);
-  if (lastButton1State == HIGH && button1State == LOW) {
-    Serial.println("[GUMB] Tipkalo 1 pritisnuto!");
-  }
-  lastButton1State = button1State;
-
-  // Obrada Echo prekida (reflektirani signal)
-  noInterrupts();
-  if (sensor_triggered) {
-    // Kopiraj vrijednost u lokalnu varijablu prije resetiranja
-    unsigned long duration = echo_duration;
-    sensor_triggered = false; // Resetiraj flag
-    interrupts(); // OMOGUĆI PREKIDE
-
-    // Izračunaj udaljenost u centimetrima (brzina zvuka = 343m/s)
-    float distance_cm = duration * 0.0343 / 2; // Pretvori mikrosekunde u centimetre
+    // Configure Timer1 in CTC mode with prescaler 1024
+    TCCR1A = 0;
+    TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);
+    OCR1A  = TIMER1_COMPARE;
+    TIMSK1 = (1 << OCIE1A);
     
-    // Ispiši samo ako je udaljenost manja od 100cm
-    if (distance_cm < 100) {
-      Serial.print("[PREKID OBRADA] Ultrazvučni senzor: ");
-      Serial.print(distance_cm);
-      Serial.println(" cm (< 100cm)");
+    Serial.begin(9600);
+    sei();  // Enable global interrupts
+    float d = measureDistance();
+    pastDistanceAlert = (d > 0 && d < ALARM_DISTANCE);
+    if (pastDistanceAlert == 1){
+      digitalWrite(LOGIC_ANALYZER_SENSOR, HIGH);
+      Serial.println("ALARM: Predmet preblizu (<100cm, LOWEST PRIORITY)!");
     }
-  } else {
-    interrupts();
-  }
-
-  // Obrada prekida gumba 2
-  noInterrupts();
-  if (button2_pressed) {
-    button2_pressed = false; // Resetiraj flag
-    interrupts(); // OMOGUĆI PREKIDE
-
-    Serial.println("[PREKID OBRADA] Tipkalo 2 pritisnuto i poslan TRIG!");
-  } else {
-    interrupts();
-  }
-
-  // Obrada timer prekida - LED treptanje
-  noInterrupts();
-  if (timer_flag) {
-    timer_flag = false; // Resetiraj flag
-    interrupts(); // OMOGUĆI PREKIDE
-
-    Serial.println("[PREKID OBRADA] Pocetak obrade Timer1 eventa...");
-    digitalWrite(LED, !digitalRead(LED)); // Toggle LED
-    Serial.println("[PREKID OBRADA] ...Zavrsetak obrade Timer1 eventa (LED toggle).");
-
-    // Ako smo u istom prolazu loop-a detektirali preklapanje, ispiši to OVDJE
-    // da se vidi da se dogodilo "unutar" logičkog bloka Timer-a.
-    if (processed_nested) {
-        Serial.println("    >>> Napomena: Preklapanje (ISR_sensor) detektirano tijekom ovog Timer ciklusa! <<<");
-    }
-  } else {
-    interrupts();
-  }
 }
 
-// serialEvent se izvršava između prolaza loop() funkcije
-void serialEvent() {
-  while (Serial.available()) {
-    char c = Serial.read();
-    Serial.print("Primljeno preko Serial: ");
-    Serial.println(c);
-  }
+/**
+ * @brief Arduino main loop.
+ *
+ * Continuously measures the distance using the ultrasonic sensor.
+ * Sets the distanceAlert flag based on the measured distance.
+ * Checks for a timer interrupt flag and handles it.
+ * Processes any pending external interrupts by blinking the corresponding LEDs.
+ * Triggers a distance alert if an object is detected too close.
+ */
+void loop(){
+    float d = measureDistance();
+    distanceAlert = (d > 0 && d < ALARM_DISTANCE);
+    if(pastDistanceAlert != distanceAlert){
+      if (distanceAlert == 0){
+        digitalWrite(LOGIC_ANALYZER_SENSOR, LOW);
+      } else {
+        digitalWrite(LOGIC_ANALYZER_SENSOR, HIGH);
+        Serial.println("ALARM: Predmet preblizu (<100cm, LOWEST PRIORITY)!");
+      }
+      pastDistanceAlert = distanceAlert;
+    }
+
+    if(timerFlag){ 
+        handleTimerInterrupt(); 
+    }
+    
+    handleBlinking();
+    
+    triggerDistanceAlert();
+}
+
+/**
+ * @brief Timer1 Compare Match Interrupt Service Routine.
+ *
+ * This ISR is called when Timer1 reaches the compare value.
+ * It toggles the logic analyzer timer pin, sets the timer flag, and updates the last timer time.
+ */
+ISR(TIMER1_COMPA_vect){
+    digitalWrite(LOGIC_ANALYZER_TIMER, HIGH);
+    timerFlag = true;
+    lastTimerTime = millis();
+    digitalWrite(LOGIC_ANALYZER_TIMER, LOW);
+}
+
+/**
+ * @brief Handles the timer interrupt event.
+ *
+ * Activates the LED for timer events, prints a message to the Serial Monitor,
+ * waits for a short duration, then turns off the LED and resets the timer flag.
+ */
+void handleTimerInterrupt(){
+    digitalWrite(LED_Timer, HIGH);
+    Serial.println("TIMER INTERRUPT (HIGHEST PRIORITY)");
+    delay(200);
+    digitalWrite(LED_Timer, LOW);
+    timerFlag = false;
+}
+
+/**
+ * @brief Interrupt Service Routine for BUTTON0 (INT0).
+ *
+ * Toggles the logic analyzer pin for INT0 and calls the generic interrupt handler.
+ */
+void ISR_INT0(){
+    digitalWrite(LOGIC_ANALYZER_PIN0, HIGH);
+    handleInterrupt(0, LED_INT0, "INT0 (HIGH priority) triggered");
+    digitalWrite(LOGIC_ANALYZER_PIN0, LOW);
+}
+
+/**
+ * @brief Interrupt Service Routine for BUTTON1 (INT1).
+ *
+ * Toggles the logic analyzer pin for INT1 and calls the generic interrupt handler.
+ */
+void ISR_INT1(){ 
+    digitalWrite(LOGIC_ANALYZER_PIN1, HIGH);
+    handleInterrupt(1, LED_INT1, "INT1 (MEDIUM priority) triggered");
+    digitalWrite(LOGIC_ANALYZER_PIN1, LOW);
+}
+
+/**
+ * @brief Interrupt Service Routine for BUTTON2 (INT2).
+ *
+ * Toggles the logic analyzer pin for INT2 and calls the generic interrupt handler.
+ */
+void ISR_INT2(){
+    digitalWrite(LOGIC_ANALYZER_PIN2, HIGH);
+    handleInterrupt(2, LED_INT2, "INT2 (LOW priority) triggered");
+    digitalWrite(LOGIC_ANALYZER_PIN2, LOW);
+}
+
+/**
+ * @brief Common interrupt handler.
+ *
+ * Debounces the interrupt by checking the elapsed time since the last interrupt.
+ * If the debounce delay has passed, the corresponding interrupt flag is set,
+ * the current time is saved, and a message is printed to the Serial Monitor.
+ *
+ * @param i Interrupt index (0 for INT0, 1 for INT1, 2 for INT2).
+ * @param led LED pin associated with this interrupt.
+ * @param msg Message to print indicating which interrupt was triggered.
+ */
+void handleInterrupt(int i, int led, const char* msg){
+    if((millis() - lastInterruptTime[i]) < DEBOUNCE_DELAY) return;
+    lastInterruptTime[i] = millis();
+    intFlag[i] = true;
+    Serial.println(msg);
+}
+
+/**
+ * @brief Processes external interrupt flags by blinking LEDs.
+ *
+ * Iterates over the interrupt flags array; if a flag is set, calls blinkLed for the
+ * corresponding LED and then clears the flag.
+ */
+void handleBlinking(){
+    for(int i = 0; i < 3; i++){
+        if(intFlag[i]){
+            blinkLed((i == 0) ? LED_INT0 : (i == 1) ? LED_INT1 : LED_INT2);
+            intFlag[i] = false;
+        }
+    }
+}
+
+/**
+ * @brief Blinks the specified LED for 1 second.
+ *
+ * Toggles the LED state every BLINK_INTERVAL milliseconds for a duration of 1 second.
+ *
+ * @param led Pin number of the LED to blink.
+ */
+void blinkLed(int led){
+    unsigned long start = millis();
+    while(millis() - start < 1000){
+        digitalWrite(led, !digitalRead(led));
+        delay(BLINK_INTERVAL);
+    }
+    digitalWrite(led, LOW);
+}
+
+/**
+ * @brief Measures distance using an ultrasonic sensor.
+ *
+ * Sends a trigger pulse on TRIG_PIN, then measures the duration of the echo on ECHO_PIN.
+ * Converts the pulse duration to a distance in centimeters.
+ *
+ * @return Measured distance in centimeters, or -1 if no valid echo is received.
+ */
+float measureDistance(){
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    
+    long t = pulseIn(ECHO_PIN, HIGH, 30000);
+    return (t > 0) ? t / 58.0 : -1;
+}
+
+/**
+ * @brief Triggers a distance alert if an object is detected too close.
+ *
+ * If the measured distance is within the alarm threshold, this function toggles the sensor LED
+ * at 200 ms intervals and prints an alarm message. Otherwise, it ensures the LED is turned off.
+ */
+void triggerDistanceAlert() {
+    static unsigned long prev = 0;
+    if (distanceAlert) {
+        if (millis() - prev >= 200) {
+            prev = millis();
+            digitalWrite(LED_Sensor, !digitalRead(LED_Sensor));
+        }
+    } else {
+        digitalWrite(LED_Sensor, LOW);  // Turn off LED when distance is safe
+    }
 }
